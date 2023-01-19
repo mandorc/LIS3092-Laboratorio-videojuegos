77766479a1c41a10daac798ec4ebd4f16071614a
Armando <95893860+mandorc@users.noreply.github.com>
2023-01-19 10:49:38 -0600

    Lab1_update
    Parallax example with Mario

---
+++ Laboratorio/Laboratorio 1/lab-parallax/lab-parallax/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace lab_parallax
+{
+    partial class PARALLAX
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.TIMER = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Location = new System.Drawing.Point(285, 190);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(70, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // TIMER
+            // 
+            this.TIMER.Enabled = true;
+            this.TIMER.Interval = 10;
+            this.TIMER.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PARALLAX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 303);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "PARALLAX";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PARALLAX_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PARALLAX_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.PARALLAX_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer TIMER;
+    }
+}
+
---
+++ Laboratorio/Laboratorio 1/lab-parallax/lab-parallax/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_parallax
+{
+
+    public partial class PARALLAX : Form
+    {
+
+        //global variables
+        int motion1 = 2;
+        int motion2 = 4;
+        int motion3 = 8;
+        int motion4 = 16;
+
+        int width = 688;
+
+        int l1_x1, l1_x2, l2_x1, l2_x2, l3_x1, l3_x2;
+
+        Bitmap layer0, layer1, layer2, layer3;
+        bool right, hold = true;
+
+        static Graphics g;
+
+        private void BackgroundMove()
+        {
+
+            if (l1_x1 < -width) { l1_x1 = width - motion1; }
+            l1_x1 -= motion1; l1_x2 -= motion1;
+            if (l1_x2 < -width) { l1_x2 = width - motion1; }
+
+            if (l2_x1 < -width) { l2_x1 = width - motion2; }
+            l2_x1 -= motion2; l2_x2 -= motion2;
+            if (l2_x2 < -width) { l2_x2 = width - motion2; }
+
+            if (l3_x1 < -width) { l3_x1 = width - motion3; }
+            l3_x1 -= motion3; l3_x2 -= motion3;
+            if (l3_x2 < -width) { l3_x2 = width - motion3; }
+
+            Invalidate();
+        }
+        public PARALLAX()
+        {
+            InitializeComponent();
+
+            layer1 = Resource1.L1_1_;
+            layer2 = Resource1.L2_1_;
+            layer3 = Resource1.L3_1_;
+
+            l1_x1 = l2_x1 = l3_x1 = 0;
+            l1_x2 = l2_x2 = l3_x2 = width;
+
+            player.Image = Resource1.Mhold_1_;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (right)
+                BackgroundMove();
+        }
+
+        private void PARALLAX_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Right & hold)
+            {
+                right = true;
+                hold = false;
+                player.Image = Resource1.running_1_;
+            }
+        }
+
+        private void PARALLAX_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Right & !hold)
+            {
+                right = false;
+                hold = true;
+                player.Image = Resource1.Mhold_1_;
+            }
+        }
+
+        private void PARALLAX_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+
+            g.DrawImage(layer1, l1_x1, 0, layer1.Width, this.Height - 50);
+            g.DrawImage(layer1, l1_x2, 0, layer1.Width, this.Height - 50);
+
+            g.DrawImage(layer2, l2_x1, 50, layer1.Width, this.Height - 150);
+            g.DrawImage(layer2, l2_x2, 50, layer1.Width, this.Height - 150);
+
+            g.DrawImage(layer3, l3_x1, 0, layer1.Width, this.Height - 50);
+            g.DrawImage(layer3, l3_x2, 0, layer1.Width, this.Height - 50);
+        }
+    }
+}
